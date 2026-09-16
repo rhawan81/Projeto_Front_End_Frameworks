30c28fd3ff3cccaf5a25ffe4fb53d8468d94e6cd
Ariely Lopes <arielylopesb16@gmail.com>
2026-09-15 22:37:40 -0300

    feat: completa CRUD de matérias
    Adiciona edição e exclusão de matérias, seleção e edição de cores, validação de nomes duplicados e persistência dos dados no localStorage.

--- src/pages/materias.jsx
+++ src/pages/materias.jsx
@@ -1,22 +1,26 @@
 import { useEffect, useState } from "react";
+
 import { carregarMaterias, salvarMaterias } from "../data/storage";
 
 function Materias() {
     const [materias, setMaterias] = useState([]);
+    const [mostrarFormulario, setMostrarFormulario] = useState(false);
+    const [nomeMateria, setNomeMateria] = useState("");
+    const [corSelecionada, setCorSelecionada] = useState("#9b5cff");
+    const [materiaEditando, setMateriaEditando] = useState(null);
 
     useEffect(() => {
         setMaterias(carregarMaterias());
     }, []);
 
     function adicionarMateria() {
-        const nome = prompt("Digite o nome da matéria:");
+        alert("Nome digitado: " + nomeMateria);
 
-        if (!nome || !nome.trim()) {
-            return;
-        }
-
-        const nomeNormalizado = nome.trim();
-
+        if (!nomeMateria.trim()) {
+        return;
+    }
+        const nomeNormalizado = nomeMateria.trim();
+       
         const jaExiste = materias.some(
             (materia) =>
                 materia.nome.toLowerCase() === nomeNormalizado.toLowerCase()
@@ -30,29 +34,189 @@ function Materias() {
         const novaMateria = {
             id: Date.now(),
             nome: nomeNormalizado,
+            cor: corSelecionada || null,
         };
 
         const novasMaterias = [...materias, novaMateria];
 
         setMaterias(novasMaterias);
         salvarMaterias(novasMaterias);
+
+        setNomeMateria("");
+        setCorSelecionada("#9b5cff");
+        setMostrarFormulario(false);
     }
 
+//vai salvar a materia editada
+    function editarMateria(id) {
+    const materia = materias.find(
+        (materia) => materia.id === id
+    );
+
+    if (!materia) {
+        return;
+    }
+
+    setMateriaEditando(materia);
+}
+
+function salvarEdicaoMateria() {
+    if (!materiaEditando.nome.trim()) {
+        return;
+    }
+
+    const nomeNormalizado = materiaEditando.nome.trim();
+
+    const jaExiste = materias.some(
+        (materia) =>
+            materia.id !== materiaEditando.id &&
+            materia.nome.toLowerCase() === nomeNormalizado.toLowerCase()
+    );
+
+    if (jaExiste) {
+        alert("Essa matéria já está cadastrada.");
+        return;
+    }
+
+    const materiasAtualizadas = materias.map(
+        (materia) =>
+            materia.id === materiaEditando.id
+                ? {
+                    ...materiaEditando,
+                    nome: nomeNormalizado,
+                }
+                : materia
+    );
+
+    setMaterias(materiasAtualizadas);
+    salvarMaterias(materiasAtualizadas);
+
+    setMateriaEditando(null);
+}
+
+//vai excluir a materia selecionada
+    function excluirMateria(id) { 
+    const confirmar = window.confirm(
+        "Tem certeza que deseja excluir esta matéria?"
+    );
+
+    if (!confirmar) {
+        return;
+    }
+
+    const materiasAtualizadas = materias.filter(
+        (materia) => materia.id !== id
+    );
+
+    setMaterias(materiasAtualizadas);
+    salvarMaterias(materiasAtualizadas);
+}
+
     return (
-        <main>
+        <main className="pagina-materias">
             <h1>Matérias</h1>
 
-            <button onClick={adicionarMateria}>
+            <button
+                className="botao-nova-materia"
+                onClick={() => setMostrarFormulario(true)}
+            >
                 + Nova matéria
             </button>
+
+            {mostrarFormulario && (
+    <div className="formulario-materia">
+        <input
+            type="text"
+            placeholder="Nome da matéria"
+            value={nomeMateria}
+            onChange={(e) => setNomeMateria(e.target.value)}
+        />
+
+        <label>
+            Escolha uma cor para a matéria:
+            <input
+                type="color"
+                value={corSelecionada}
+                onChange={(e) => setCorSelecionada(e.target.value)}
+            />
+        </label>
+
+        <button onClick={adicionarMateria}>
+            Salvar matéria
+        </button>
+    </div>
+)}
+
+{materiaEditando && (
+    <div className="formulario-materia">
+        <input
+            type="text"
+            placeholder="Nome da matéria"
+            value={materiaEditando.nome}
+            onChange={(e) =>
+                setMateriaEditando({
+                    ...materiaEditando,
+                    nome: e.target.value,
+                })
+            }
+        />
+
+        <label>
+            Escolha uma cor para a matéria:
+            <input
+                type="color"
+                value={materiaEditando.cor || "#9b5cff"}
+                onChange={(e) =>
+                    setMateriaEditando({
+                        ...materiaEditando,
+                        cor: e.target.value,
+                    })
+                }
+            />
+        </label>
+
+        <button onClick={salvarEdicaoMateria}>
+            Salvar alterações
+        </button>
+    </div>
+)}
 
             {materias.length === 0 ? (
                 <p>Nenhuma matéria cadastrada.</p>
             ) : (
-                <ul>
+                <ul className="lista-materias">
                     {materias.map((materia) => (
-                        <li key={materia.id}>
-                            {materia.nome}
+                        <li
+                            className="item-materia"
+                            key={materia.id}
+                        >
+                    <span
+                    className="nome-materia"
+                        style={{
+                            backgroundColor: materia.cor || "transparent",
+                            padding: "4px 10px",
+                            borderRadius: "8px",
+                            opacity: 0.60,
+                        }}
+                    >
+                        {materia.nome}
+                    </span>
+
+                            <div className="acoes-materia">
+                                <button
+                                    className="botao-editar"
+                                    onClick={() => editarMateria(materia.id)}
+                                >
+                                    Editar
+                                </button>
+
+                                <button
+                                     className="botao-excluir"
+                                    onClick={() => excluirMateria(materia.id)}
+                                >
+                                    Excluir
+                                </button>
+                            </div>
                         </li>
                     ))}
                 </ul>
